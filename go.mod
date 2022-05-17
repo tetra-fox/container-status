8ed8659231654048757267782a76b6c3965f980f
Tetra <me@tetra.cool>
2022-05-17 18:00:58 -0400

    init

---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/tetra-fox/container-status
+
+go 1.18
+
+require (
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.16+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+type responseContainer struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+}
+
+type response struct {
+	Containers []responseContainer `json:"containers"`
+	Time       int64 `json:"time"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", listContainers)
+	router.GET("/:name", getContainerByName)
+
+	router.Run("localhost:3621")
+}
+
+func getContainers() ([]byte, error) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err;
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		return nil, err;
+	}
+
+	sanitizedContainers := make([]responseContainer, len(containers))
+	for i, container := range containers {
+		sanitizedContainers[i] = responseContainer{container.Names[0][1:], container.State}
+	}
+
+	responseJson, err := json.Marshal(response{sanitizedContainers, time.Now().UnixMilli()})
+	if err != nil {
+		return nil, err;
+	}
+
+	return responseJson, nil;
+}
+
+func listContainers(c *gin.Context) {
+	responseJson, err := getContainers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Data(http.StatusOK, gin.MIMEJSON, responseJson)
+}
+
+func getContainerByName(c *gin.Context) {
+	responseJson, err := getContainers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var containers response
+	err = json.Unmarshal(responseJson, &containers)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	for _, container := range containers.Containers {
+		if container.Name == c.Param("name") {
+			responseJson, err := json.Marshal(response{[]responseContainer{container}, time.Now().UnixMilli()})
+			if err != nil {
+				panic(err)
+			}
+			c.Data(http.StatusOK, gin.MIMEJSON, responseJson)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "container not found"})
+}
